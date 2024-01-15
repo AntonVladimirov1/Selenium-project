fe7449e928fd876cbb12e300c51c96d0bb32b939
Antonio_Git <l.a.v7232@gmail.com>
2024-01-15 16:12:11 -0600

    Automation

--- src/Test/java/com/cydeo/Antonio/devMH_order.java
+++ src/Test/java/com/cydeo/Antonio/devMH_order.java
@@ -7,15 +7,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class devMH_order {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         WebDriverManager.chromedriver().setup();
         // Create an instance of browser
         WebDriver driver = new ChromeDriver();
 
-        driver.get(ConfigReader.getProperty("devMHloginURL"));                                                                                       //Login page
+        driver.get(ConfigReader.getProperty("devMHloginURL"));                                                                                 //Login page
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.manage().window().maximize();
         driver.findElement(By.xpath("//*[@id=\"Username\"]")).sendKeys(ConfigReader.getProperty("devMHusername"));                 //Username
@@ -25,14 +27,14 @@ public class devMH_order {
 
         driver.navigate().to(ConfigReader.getProperty("devMHhomeURL"));                                                                         //Home button
         driver.findElement(By.xpath("//*[@id=\"SearchModel_FirstLocation_ServiceAddress\"]")).sendKeys("12100 Northpointe Blvd Tomball, Texas 77377, United States");                                                                                                         //Address
-        driver.findElement(By.xpath("//*[@id=\"SearchModel_FirstLocation_JobDate\"]")).click();                                    //Date field
-        driver.findElement(By.xpath("//*[@id=\"ui-datepicker-div\"]/table/tbody/tr[4]/td[2]/a")).click();                          //Exact date
+        driver.findElement(By.xpath("//*[@id=\"SearchModel_FirstLocation_JobDate\"]")).click();                                   //Date field
+        driver.findElement(By.xpath("//td[@data-handler='selectDay']/a[.='"+dateToday+"']")).click();                            //Current date
         driver.findElement(By.xpath("//*[@id=\"SearchModel_FirstLocation_JobTime_Mobile\"]")).sendKeys("Morning");      //Time
         driver.findElement(By.xpath("//button[.='Search']")).click();                                                               //Search
         //driver.navigate().refresh();
         //BrowserUtils.sleep(1);
 
-        driver.findElement(By.xpath("//a[text()='TexasBest']")).click();                                                         //Provider choose
+        driver.findElement(By.xpath("//a[text()='TexasBest']")).click();                                                         //Provider select
         driver.findElement(By.xpath("//*[@id=\"DetailsOfMove\"]")).sendKeys("dfgdfgdfg");                               //Details
         driver.findElement(By.xpath("//*[@id=\"submitButton\"]")).click();                                                         //Add to Cart
         //BrowserUtils.sleep(1);
@@ -50,6 +52,11 @@ public class devMH_order {
 
         //==========================================>> Confirm Order <<==================================================================================
 
-        //driver.findElement(By.xpath("//button[@class='button submit-button']")).click();                                       //Submit
+        driver.findElement(By.xpath("//button[@class='button submit-button']")).click();                                       //Submit
     }
+    // Created String variable to pick current date
+    static DateTimeFormatter df = DateTimeFormatter.ofPattern("dd");
+    static LocalDate today = LocalDate.now();
+    static String dateToday = df.format(today);
+
 }
